b4e1fb7486a4b84a01d53a5db2a4f8b284ce4a86
hypo <s210101120@stu.cqupt.edu.cn>
2023-03-02 12:11:09 +0800

    2.27显示登陆内容

---
+++ src/main/java/com/nowcoder/community/dao/LoginTicketMapper.java
@@ -0,0 +1,32 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.LoginTicket;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface LoginTicketMapper {
+
+    @Insert({
+            "insert into login_ticket(user_id,ticket,status,expired) ",
+            "values(#{userId},#{ticket},#{status},#{expired})"
+    })
+    @Options(useGeneratedKeys = true, keyProperty = "id")/*自动生成主键并注入给LoginTicket*/
+    int insertLoginTicket(LoginTicket loginTicket);
+
+    @Select({
+            "select id,user_id,ticket,status,expired ",
+            "from login_ticket where ticket=#{ticket}"
+    })
+    LoginTicket selectByTicket(String ticket);
+
+    @Update({
+            "<script>",
+            "update login_ticket set status=#{status} where ticket=#{ticket} ",
+            "<if test=\"ticket!=null\"> ",
+            "and 1=1 ",
+            "</if>",
+            "</script>"
+    })/*在Mapper里写动态sql*/
+    int updateStatus(String ticket, int status);
+
+}
